3561939454614b08ddb4171077feb7227750e7cd
bigpumkin <86058322+bigpumkin@users.noreply.github.com>
2022-12-17 00:02:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <cmath>
+#include <string>
+
+
+#include "include/module/osc.hpp"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	VideoMode SCREEN_SIZE = sf::VideoMode::getDesktopMode();
+	// создаем окно
+	RenderWindow window(VideoMode(SCREEN_SIZE), "");
+
+	// приложение будет работать, пока окно открыто
+	while (window.isOpen())
+	{
+		// проверяем все события окна, которые были запущены с предыдущей итерации цикла
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// сработало событие закрытия окна - закроем его
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear(sf::Color(3, 71, 105));
+        
+        Oscillator osc1;
+
+		window.display();
+	}
+
+	return 0;
+}
